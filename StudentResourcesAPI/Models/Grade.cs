c1a72e4311eefcb73f9c7159f39ae92ba1a8ca92
phuocding <phuocding@gmail.com>
2018-12-29 19:36:41 +0700

    updated grade model for StudentResourceAPI

--- StudentResourcesAPI/Models/Grade.cs
+++ StudentResourcesAPI/Models/Grade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,31 @@ namespace StudentResourcesAPI.Models
 {
     public class Grade
     {
+        [Key]
         public int StudentId { get; set; }
         public int SubjectId { get; set; }
-        public float GradeNumber { get; set; }
-        public int GradeType { get; set; }
+        public float Mark { get; set; }
+        public GradeType GradeType { get; set; }
         public GradeStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public Account Account { get; set; }
+        public Subject Subject { get; set; }
+
+        // create constructor
     }
 
     public enum GradeStatus
     {
-        Active = 1,
-        Deactive = 0,
+        Failed = 0,
+        Passed = 1,
+    }
+
+    public enum GradeType
+    {
+        AssignmentGrade = 1,
+        PracticeGrade = 2,
     }
 }
