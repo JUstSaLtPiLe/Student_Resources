a9986f14422d7a5b4becf8c6f6543116590a8266
phuocding <phuocding@gmail.com>
2018-12-29 18:28:28 +0700

    added grade model for StudentResourceAPI

---
+++ StudentResourcesAPI/Models/Grade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentResourcesAPI.Models
+{
+    public class Grade
+    {
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public float GradeNumber { get; set; }
+        public int GradeType { get; set; }
+        public GradeStatus Status { get; set; }
+    }
+
+    public enum GradeStatus
+    {
+        Active = 1,
+        Deactive = 0,
+    }
+}
