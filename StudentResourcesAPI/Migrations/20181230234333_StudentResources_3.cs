1e8f74f618a81f751e0b701b466afd68904d6e69
JUstSaLtPiLe <vu.tienphuc97@gmail.com>
2018-12-31 09:43:52 +0700

    done CRUD account lan 1

---
+++ StudentResourcesAPI/Migrations/20181230234333_StudentResources_3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentResourcesAPI.Migrations
+{
+    public partial class StudentResources_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Account");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Account",
+                newName: "AccountId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountId",
+                table: "Account",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Account",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
