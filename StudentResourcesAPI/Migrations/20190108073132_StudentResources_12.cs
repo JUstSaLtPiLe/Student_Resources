88a404d920cac8c57e7bffcaf0f05e9ada0472b7
JUstSaLtPiLe <vu.tienphuc97@gmail.com>
2019-01-08 15:27:21 +0700

    publish api len azure

---
+++ StudentResourcesAPI/Migrations/20190108073132_StudentResources_12.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentResourcesAPI.Migrations
+{
+    public partial class StudentResources_12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
